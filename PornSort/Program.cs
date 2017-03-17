5dde3f0a9d07a88fadf8b4760c0109bec9accced
MichelleCKing <michellecking@spamhereplease.com>
2017-03-17 16:21:02 -0400

    init commit

---
+++ PornSort/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Threading;
+
+namespace PornSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sortedImagesDirectory = new DirectoryInfo("sorted");
+            var unsortedImagesDirectory = new DirectoryInfo("images");
+            var apiKey = "API KEY";
+            var apiEndpoint = String.Format("https://api.haystack.ai/api/image/analyzeadult?output=json&apikey={0}", apiKey);
+
+            using (var client = new WebClient())
+            {
+                foreach (var imagePath in unsortedImagesDirectory.EnumerateFiles())
+                {
+                    var image = File.ReadAllBytes(imagePath.FullName);
+                    var responseData = client.UploadData(apiEndpoint, "POST", image);
+                    var responseText = Encoding.UTF8.GetString(responseData);
+                    dynamic response = JsonConvert.DeserializeObject(responseText);
+                    var adultType = (String) response.adultContent.adultContentType;
+                    string sortedType;
+
+                    if (String.IsNullOrEmpty(adultType))
+                        sortedType = "clean";
+                    else
+                        sortedType = adultType;
+
+                    var sortedDirectory = Path.Combine(sortedImagesDirectory.FullName, sortedType);
+                    var sortedPath = Path.Combine(sortedDirectory, imagePath.Name);
+
+                    Directory.CreateDirectory(sortedDirectory);
+                    File.Move(imagePath.FullName, sortedPath);
+                    Thread.Sleep(50);
+                }
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# PornSort
+Using the AnalyzeAdult endpoint of the Haystack API, PornSort can categorize porn images and sort them into folders. If the image contains no nudity, it is filed under the "clean" folder in the output directory. This is only a proof of concept but it could be easily adapted to use adult content detection to any program.
